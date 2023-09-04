7fc33f53d910ac92c7ae6ee7293b75874614d312
Shreya Pramanik <100830699+ShreyaPramanik47@users.noreply.github.com>
2023-09-04 23:38:13 +0530

    Create 206. Reverse Linked List.cpp

---
+++ Easy/206. Reverse Linked List.cpp
@@ -0,0 +1,16 @@
+// LEETCODE-->
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr= head;
+        ListNode* next;
+        while(curr!=NULL){
+            next= curr->next;
+            curr->next=prev;
+            prev= curr;
+            curr= next; 
+        }
+        return prev;
+    }
+};
