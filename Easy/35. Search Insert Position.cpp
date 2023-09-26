38a90bd091ecdabfa1981e52698c39c8764664ab
Shreya Pramanik <100830699+ShreyaPramanik47@users.noreply.github.com>
2023-09-27 01:44:22 +0530

    Create 35. Search Insert Position.cpp

---
+++ Easy/35. Search Insert Position.cpp
@@ -0,0 +1,21 @@
+// T.C = O(log N)
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low=0;
+        int n = nums.size();
+        int high=n-1;
+        int ans=n;
+        while(low<=high){
+            int mid=(high+low)/2;
+            if(nums[mid]>=target){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
